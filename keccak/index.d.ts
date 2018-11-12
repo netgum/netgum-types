ac3906f389608f787074e8238e264fb8f0e1cb0b
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-11-12 08:00:43 +0100

    Fixes sha3 def

--- keccak/index.d.ts
+++ keccak/index.d.ts
@@ -1,7 +1,7 @@
 declare module "keccak" {
   import { Hash } from "crypto";
 
-  function keccak(type: "keccak256" | "keccak512"): Hash;
+  const keccak: (type: "keccak256" | "keccak512") => Hash;
 
   export = keccak;
 }
